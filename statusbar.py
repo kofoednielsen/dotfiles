a166ef6e5531f12eac7c4ebdb427fa0c2ec7a8c0
kofoednielsen <asbjorn.ko.ni@gmail.com>
2020-06-15 10:18:52 +0200

    CHanged status bar, added termitea and vim config

--- statusbar.py
+++ statusbar.py
@@ -2,7 +2,12 @@
 from datetime import datetime
 
 
+RED_B = '\033[41m'
+END = '\033[0m'
 with open('/sys/class/power_supply/BAT0/capacity') as f:
     battery = f.read().strip()
     time = datetime.now().strftime('%H:%M:%S')
-    print(f'bat: {battery}%    {time}  ')
+    warning = ''
+    if int(battery) < 10:
+        warning = '#### LOW BATTERY ####'
+    print(f'{warning.ljust(68)} bat: {battery}%    {time}  ')
