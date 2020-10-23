4c4a8452b7247f1b4c5a84398fb4daa854932c50
kofoednielsen <asbjorn.ko.ni@gmail.com>
2020-10-23 16:44:10 +0200

    🦄

--- render.py
+++ render.py
@@ -1,10 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-fontsize = 200
+fontsize = 10
 image = Image.new("RGBA", (1000, 1000), (255, 255, 255))
 draw = ImageDraw.Draw(image)
-font = ImageFont.truetype("/usr/share/fonts/ubuntu/UbuntuMono-R.ttf", 50)
 txt = "😞"
+font = ImageFont.truetype("/usr/share/fonts/twemoji/twemoji.ttf", 50)
 draw.text((10, 0), txt, (0, 0, 0), font=font)
 image.save('emoji.png')
