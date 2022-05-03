53dd5777d5d14c0bb11518ed7026855ac49ea085
askn <askn@jobindex.dk>
2022-05-03 12:53:58 +0200

    better status bar

--- statusbar.py
+++ statusbar.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
+import netifaces
 from datetime import datetime
-from typing import List
-from requests import get
 from os import system
 from pathlib import Path
+from requests import get
+from typing import List
+import os
+import psutil
 import socket
 import yaml
-import psutil
 
 
 RED_B = '\033[41m'
 END = '\033[0m'
 battery = "50"
 charging = ""
+HOME = os.getenv('HOME')
+
 with open('/sys/class/power_supply/BAT0/capacity') as f:
     battery = f.read().strip()
 with open('/sys/class/power_supply/BAT0/status') as f:
@@ -32,7 +36,7 @@ if int(battery) >= 90 and charging:
 # ░█▀▀░█░█░█░█░░░█░░█░░█░░░█▀█░░█░░█░█░█▀▄
 # ░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀
 
-emojis = yaml.load(open('/home/kofoednielsen/dotfiles/emojicator.yaml'),
+emojis = yaml.load(open(f'{HOME}/dotfiles/emojicator.yaml'),
                    Loader=yaml.FullLoader)
 default_emoji = emojis['default']
 
@@ -62,16 +66,24 @@ emoji = max(emoji_options, key=lambda e: e['from'])['emoji']
 
 
 time = datetime.now().strftime('%d/%m  %H:%M:%S')
-ip = socket.gethostbyname(socket.gethostname())
-# hex_code = f'{ord(emoji):x}'
-# path = Path(f'/home/kofoednielsen/dotfiles/emojis/{hex_code}.png')
-# if path.is_file():
-#     emoji_img = open(path, 'rb').read()
-# else:
-#     url = f"https://noto-website-2.storage.googleapis.com/emoji/emoji_u{hex_code}.png"
-#     emoji_img = get(url).content
-#     open(path, 'wb').write(emoji_img)
-# open('/home/kofoednielsen/dotfiles/background.png', 'wb').write(emoji_img)
-# system("swaymsg output '*' background /home/kofoednielsen/dotfiles/background.png center")
+ips = []
 
-print(f'{warning.ljust(60)}{ip}    {time}    {charging_emoji}{emoji}')
+for i in netifaces.interfaces(): #Will cycle through all available interfaces and check each one.
+    if i != "lo": #This will remove lo from the interfaces it checks.
+        try:
+            ips.append(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+        except:
+            pass
+
+hex_code = f'{ord(emoji):x}'
+path = Path(f'{HOME}/dotfiles/emojis/{hex_code}.png')
+if path.is_file():
+    emoji_img = open(path, 'rb').read()
+else:
+    url = f"https://noto-website-2.storage.googleapis.com/emoji/emoji_u{hex_code}.png"
+    emoji_img = get(url).content
+    open(path, 'wb').write(emoji_img)
+open(f'{HOME}/dotfiles/background.png', 'wb').write(emoji_img)
+# system("swaymsg output '*' background /home/asbi/dotfiles/background.png center")
+
+print(f'{warning.ljust(60)}{"    ".join(ips)}    {time}    {charging_emoji}{emoji}')
