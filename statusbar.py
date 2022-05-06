4b0e5662bf9a25efc5c8c9c4ee8f39821e354c7f
askn <askn@jobindex.dk>
2022-05-06 16:53:31 +0200

    backgroundgit add kitty.config git add kitty.config

--- statusbar.py
+++ statusbar.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-import netifaces
 from datetime import datetime
 from os import system
 from pathlib import Path
 from requests import get
+from subprocess import Popen
+from time import sleep
 from typing import List
-import os
+import netifaces
+import os, signal
 import psutil
 import socket
 import yaml
@@ -15,7 +17,9 @@ RED_B = '\033[41m'
 END = '\033[0m'
 battery = "50"
 charging = ""
-HOME = os.getenv('HOME')
+HOME = Path(os.getenv('HOME'))
+TMP = Path("/tmp/")
+DOTFILES_FOLDER = HOME / 'dotfiles'
 
 with open('/sys/class/power_supply/BAT0/capacity') as f:
     battery = f.read().strip()
@@ -36,7 +40,7 @@ if int(battery) >= 90 and charging:
 # ░█▀▀░█░█░█░█░░░█░░█░░█░░░█▀█░░█░░█░█░█▀▄
 # ░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀
 
-emojis = yaml.load(open(f'{HOME}/dotfiles/emojicator.yaml'),
+emojis = yaml.load(open(DOTFILES_FOLDER / "emojicator.yaml"),
                    Loader=yaml.FullLoader)
 default_emoji = emojis['default']
 
@@ -67,23 +71,49 @@ emoji = max(emoji_options, key=lambda e: e['from'])['emoji']
 
 time = datetime.now().strftime('%d/%m  %H:%M:%S')
 ips = []
-
 for i in netifaces.interfaces(): #Will cycle through all available interfaces and check each one.
     if i != "lo": #This will remove lo from the interfaces it checks.
         try:
-            ips.append(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+            ips.append((i, netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']))
         except:
             pass
+if_map = { 'wlp0s20f3': 'wifi', 'enx186571fd5e15': 'lan' }
+ip_str = "    ".join(f'{if_map.get(i, i)}: {ip}' for i, ip in ips)
 
+# PRINT OUTPUT
+print(f'{warning.ljust(45)}{ip_str}    {time}    {charging_emoji}{emoji}')
+
+
+# HANDLE BACKGROUND
 hex_code = f'{ord(emoji):x}'
-path = Path(f'{HOME}/dotfiles/emojis/{hex_code}.png')
-if path.is_file():
-    emoji_img = open(path, 'rb').read()
+emoji_path = DOTFILES_FOLDER / f"emojis/{hex_code}.png"
+if emoji_path.is_file():
+    emoji_img = open(emoji_path, 'rb').read()
 else:
-    url = f"https://noto-website-2.storage.googleapis.com/emoji/emoji_u{hex_code}.png"
+    url = f"https://github.com/googlefonts/noto-emoji/raw/main/png/512/emoji_u{hex_code}.png"
     emoji_img = get(url).content
-    open(path, 'wb').write(emoji_img)
-open(f'{HOME}/dotfiles/background.png', 'wb').write(emoji_img)
-# system("swaymsg output '*' background /home/asbi/dotfiles/background.png center")
+    open(emoji_path, 'wb').write(emoji_img)
 
-print(f'{warning.ljust(60)}{"    ".join(ips)}    {time}    {charging_emoji}{emoji}')
+swaybg_pid_file = TMP / "swaybg_pid"
+swaybg_emoji_file = TMP / "swaybg_emoji"
+
+# terminate early if background is already correct
+if swaybg_emoji_file.is_file():
+    with open(swaybg_emoji_file) as f:
+        emoji = f.read()
+        if emoji == hex_code:
+            exit()
+         
+
+swaybg = Popen(["swaybg", "-i", emoji_path, "-m", "center", "-c", "#005063"])
+sleep(0.1)
+if swaybg_pid_file.is_file():
+    with open(swaybg_pid_file) as f:
+        pid = int(f.read())
+        try:
+            os.kill(pid, signal.SIGINT)
+        except: 
+            pass
+open(swaybg_pid_file, "w").write(str(swaybg.pid))
+open(swaybg_emoji_file, "w").write(str(hex_code))
+
